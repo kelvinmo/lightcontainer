1c0b7a7e70e25cfbf96988255887d35f1e095276
Kelvin Mo <kmo@users.sourceforge.net>
2021-08-15 10:23:12 +1000

    Basic resolvers

---
+++ src/Resolvers/ResolverInterface.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * LightContainer
+ *
+ * Copyright (C) Kelvin Mo 2021
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace LightContainer\Resolvers;
+
+use LightContainer\Container;
+
+/**
+ * An interface for all resolvers.
+ * 
+ * A *resolver* is a class that returns an object or some other value when
+ * the {@link resolve()} method is called.
+ * Examples of ways by which a resolver can resolve to a value include:
+ * 
+ * - Creating a new instance of a class
+ * - Retrieving from a stored value
+ * - Calling the parent container to resolve the value from another
+ *   identifier
+ */
+interface ResolverInterface {
+    /**
+     * Resolves an object or value
+     * 
+     * @param Container $container the LightContainer instance
+     * @return mixed the resolved object or value
+     * @throws NotFoundException if the resolver cannot instantiate the
+     * object
+     */
+    public function resolve(Container $container);
+}
+
+?>
