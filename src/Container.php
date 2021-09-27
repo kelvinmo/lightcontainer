66c76385d924a941155a65d4c00c1be5b7fda41c
Kelvin Mo <kmo@users.sourceforge.net>
2021-09-27 11:45:13 +1000

    Implementation of Container::populate()

--- src/Container.php
+++ src/Container.php
@@ -114,6 +114,20 @@ class Container implements LightContainerInterface {
         return $resolver;
     }
 
+    public function populate(string $class_name, array $exclude = []): ResolverInterace {
+        $resolver = new ClassResolver($class_name);
+
+        $refl = new \ReflectionClass($class_name);
+        $interfaces = $refl->getInterfaceNames();
+        foreach ($interfaces as $interface) {
+            if (!in_array($interface, $exclude)) {
+                $this->resolvers[$interface] = $resolver;
+            }
+        }
+
+        return $resolver;
+    }
+
     /**
      * Remove a resolver from a container
      * 
